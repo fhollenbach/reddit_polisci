18b5123cedd0f686244bd5389b9fbe8a59c221bc
fhollenbach <florianhollenbach@Florians-MacBook-Pro.local>
2018-02-04 23:25:27 -0600

    fetching

--- download_reddit.py
+++ download_reddit.py
@@ -1,15 +1,33 @@
+
 import praw
-r = praw.Reddit(user_agent='"polsci_test by /u/politics_science"')
 r = praw.Reddit(client_id='7mGIoZ0ZPzUuGw',
                      client_secret=	'E3ATC9jPeHCRuFNbT4pjz7dFwXw',
                      user_agent='polsci_test by /u/politics_science')
-
-
-for submission in r.subreddit('learnpython').hot(limit=10):
-    print(submission.title)
 
 
 
 subreddit = r.subreddit('nfl')
 for comment in r.subreddit('nfl').stream.comments():
     print(comment)
+
+
+
+
+def getSubComments(comment, allComments, verbose=True):
+  allComments.append(comment)
+  if not hasattr(comment, "replies"):
+    replies = comment.comments()
+    if verbose: print("fetching (" + str(len(allComments)) + " comments fetched total)")
+  else:
+    replies = comment.replies
+  for child in replies:
+    getSubComments(child, allComments, verbose=verbose)
+
+
+def getAll(r, submissionId, verbose=True):
+  submission = r.submission(submissionId)
+  comments = submission.comments
+  commentsList = []
+  for comment in comments:
+    getSubComments(comment, commentsList, verbose=verbose)
+  return commentsList
