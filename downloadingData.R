7c5439d0d62b428f9570c596d761d4f685b86433
fhollenbach <florianhollenbach@Florians-MacBook-Pro.local>
2018-02-16 23:20:21 -0600

    download in R2

--- downloadingData.R
+++ downloadingData.R
@@ -24,5 +24,6 @@ for(year in years){
 
   res <- query_exec(sql, project = project, useLegacySql = FALSE, max_pages = Inf)
   save(res, file = paste("~/Dropbox/RedditData/Downloads/nflsubreddit",year,month,".rda",sep = "_"))
+  rm(res)
   }
 }
